7d9ccbc72a7299b06ac8d8b365b2471680dd6807
Dario Meneses <dariomeneses1995@gmail.com>
2024-07-10 21:54:12 -0500

    Tipos de datos y variables en python

---
+++ Tipos_de_datos.py
@@ -0,0 +1,17 @@
+# tipos de datos
+#Cadenas de texto
+"String"
+'string'
+"""String"""
+'''String'''
+"""Mis datos son
+Nombre: Dario Meneses
+Edad: 28 años
+"""
+#Tipos de datos numericos
+5    #Entero
+5.3  #Fltante
+
+#Tipo de datos boleanos
+True
+False 
---
+++ Variables/Tipo_de_variables.py
@@ -0,0 +1,9 @@
+#Tipo de variables
+nombre = "Dario"
+bienvenida = f"Hola {nombre} feliz noche" #concatenar textos y numeros
+print(bienvenida)
+print("oso" not in bienvenida)
+print("oso" in bienvenida)
+
+nombreCompletoDelEstudiante = "Dario Meneses"  #Camelcase
+nombre_completo_del_estdiante = "Dario Meneses" #Snakecase
