69a4f30fd9a0f39687a615250cba53c4bcc62279
Dario Meneses <dariomeneses1995@gmail.com>
2024-07-15 18:43:42 -0500

    Nuevo repositorio fundamentos

---
+++ fundamentos.py
@@ -0,0 +1,14 @@
+name = " Dario "
+print(type(name))
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+last_name = "Meneses"
+print(name * 5)
+print(len(name))
+print(name.lower()) #minusculas
+print(name.upper()) #mayusculas
+print(name.strip()) #longitud de la variable
+print(name + "" + last_name) #contatenar
