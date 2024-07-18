292da8a849c7f739be68917876633daa0aadde9c
Dario Meneses <dariomeneses1995@gmail.com>
2024-07-17 21:12:16 -0500

    Ejercicios de operadores de entrada y salida

---
+++ operadores_de_entrada_y_salida.py
@@ -0,0 +1,9 @@
+nombre = input("¿Cual es tu nombre?") #input toma los datos como strings
+print(nombre)
+print(type(nombre))
+edad = int(input("¿Cual es tu edad?")) #Se precede int o float segun sea el dato para numeros
+print(edad)
+print(type(edad))
+estatura = float(input("¿Cual es tu estatura?"))
+print(estatura)
+print(type(estatura))
