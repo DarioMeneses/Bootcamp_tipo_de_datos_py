61ba6a83675882dd8833ba789e018d49f391fa34
Dario Meneses <dariomeneses1995@gmail.com>
2024-07-21 17:42:30 -0500

    Operadores aritmeticos en phyton

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,21 @@
+#Sumas y Restas
+suma = 2024+2000
+print(suma)
+resta = 2024-1995
+print(resta)
+#Multiplicacion y division
+multiplicacion = 23 * 7
+print(multiplicacion)
+division = 8 / 2
+print(division)
+#Potenciacion
+potencia = 2 ** 3
+print(potencia)
+#Division entera
+division_e = 12 // 4
+print(division_e)
+#Resto o modulo
+resto = 12 % 4
+print(resto)
+tipo_de_dato = type(division_e)
+print(tipo_de_dato)
