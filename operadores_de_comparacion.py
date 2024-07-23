358dcbfbdf44bdfab283cee39501c1e720033290
Dario Meneses <dariomeneses1995@gmail.com>
2024-07-23 01:20:09 -0500

    Operadores de comparacion

---
+++ operadores_de_comparacion.py
@@ -0,0 +1,25 @@
+# Operadores de comparacion
+es_igual_que = 5 == 5
+print(es_igual_que)
+es_diferente = 5 != 6
+print(es_diferente)
+mayor_que = 5 > 6
+print(mayor_que)
+mayor_o_igual_que = 5 >= 6
+print(mayor_o_igual_que)
+menor_que = 5 < 6
+print(menor_que)
+menor_o_igual = 5 <= 6
+print(menor_o_igual)
+
+# Calculos combinados
+a = 10
+b = 10
+c = 20
+comparacion = a + b == c
+print(comparacion)
+
+# comparar usuarios
+contrasena_almacenada = "12345678"
+contrasena_escrita = input("Contraseña: ")
+print(contrasena_almacenada == contrasena_escrita)
