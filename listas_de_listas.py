872e560585b2522403027c0050c4c00044a0d165
Dario Meneses <dariomeneses1995@gmail.com>
2024-08-05 21:00:54 -0500

    Matriz, listas de lstas

---
+++ listas_de_listas.py
@@ -0,0 +1,20 @@
+def matrices(): 
+ matriz = [[1,2,3],
+           [4,5,6],
+           [7,8,9]]
+ for matri in matriz:
+    print(matri)
+
+matrices()
+
+matriz = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+print(matriz)
+print(matriz[0])
+
+numbers = (1,2,3,4,5,6)
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+
