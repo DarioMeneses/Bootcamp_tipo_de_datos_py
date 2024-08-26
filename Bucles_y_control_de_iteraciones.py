8cb4a18d39be6a0f88a5d2375a393329293bb5a4
Dario Meneses <dariomeneses1995@gmail.com>
2024-08-26 18:50:12 -0500

    Bucles for y While

---
+++ Bucles_y_control_de_iteraciones.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5, 6]
+for i in numbers:
+    print("Aqui i es igual a: ", i+1)
+for i in range (3,10):
+    print(i)
+fruits = ["Manzana", "Uvas", "Cerezas", "Fresas", "Moras", "Mandarina"]
+for frut in fruits:
+    print(frut)
+    if frut == "Moras":
+       print("Fruta encontrada: Moras")
+x = 0
+while x<5:
+    if x == 3:
+        break
+    print(x)
+    x +=1
+numbers = [1, 2, 3, 4, 5, 6]
+for i in numbers:
+    if i == 3:
+        break
+    print("i es igual: ", i+1)
+        
